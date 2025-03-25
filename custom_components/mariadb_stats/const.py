d7dad376dd70842da5e8035e3b59f1f27de36bba
alexlitwinski <38701080+alexlitwinski@users.noreply.github.com>
2025-03-25 15:43:39 -0300

    Create const.py

---
+++ custom_components/mariadb_stats/const.py
@@ -0,0 +1,11 @@
+"""Constants for the MariaDB Stats integration."""
+
+DOMAIN = "mariadb_stats"
+
+# Configurações padrão
+DEFAULT_HOST = "localhost"
+DEFAULT_PORT = 3306
+DEFAULT_DATABASE = "homeassistant"
+
+# Constantes de configuração
+CONF_DATABASE = "database"
