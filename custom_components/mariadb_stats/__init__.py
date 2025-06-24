b6afd039f0bf4151aca6fbbc006ab98f4b3feca7
alexlitwinski <38701080+alexlitwinski@users.noreply.github.com>
2025-06-24 15:55:51 -0300

    Update __init__.py

--- custom_components/mariadb_stats/__init__.py
+++ custom_components/mariadb_stats/__init__.py
@@ -87,7 +87,7 @@ async def update_listener(hass: HomeAssistant, entry: ConfigEntry) -> None:
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
-    unload_ok = await hass.config_entries.async_unload_entry(entry, "sensor")
+    unload_ok = await hass.config_entries.async_forward_entry_unload(entry, "sensor")
     
     if unload_ok:
         hass.data[DOMAIN].pop(entry.entry_id)
